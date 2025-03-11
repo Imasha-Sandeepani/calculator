5da5a9d9de6d5cdb40995ac3fb9beada7810b826
Imasha <i.imasha.sandeepani@gmail.com>
2025-03-11 15:32:05 +0530

    I created a class and functions

---
+++ calculations.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        if y != 0:
+            return x / y
+        else:
+            return "Cannot divide by zero!"
+
+    def power(self, x, y):
+        return x ** y
