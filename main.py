9bb6c3a0ea335b8a9776f944e57f8856b38b57d6
Imasha <i.imasha.sandeepani@gmail.com>
2025-03-11 14:26:09 +0530

    I modified main.py file to get inputs

---
+++ main.py
@@ -0,0 +1,14 @@
+if __name__ == "_main_":
+
+    print("Simple Calculator")
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Power")
+
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
